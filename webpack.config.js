516b711eb12bec7043570da30b4490bda32c7881
Corey Hammond <coreyjhammond@gmail.com>
2021-02-09 17:56:21 -0800

    configured eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,30 @@
+const path = require('path');
+
 module.exports = {
-  entry: ["./src/index.js"],
+  entry: ['./src/index.js'],
   output: {
-    path: __dirname + "/dist",
-    publicPath: "/",
-    filename: "bundle.js",
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
   },
   devServer: {
-    contentBase: "./dist",
+    contentBase: './dist',
   },
   module: {
     rules: [
       {
-        test: /\.(.js|.jsx)$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['eslint-loader'],
       },
     ],
   },
   resolve: {
-    extensions: [".wasm", ".ts", ".tsx", ".mjs", ".cjs", ".js", ".json"],
+    extensions: ['.wasm', '.ts', '.tsx', '.mjs', '.cjs', '.js', '.json'],
   },
 };
